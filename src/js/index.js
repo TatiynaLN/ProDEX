c42e3e1fd444ab1034b42b7ef8abf39bd608e78d
TatiynaLN <tania.cis2011@yandex.ru>
2021-11-13 13:04:38 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 import './libs/swiper-bundle.min.js'; // Swiper-slider
 import accordion from './modules/accordion.js'; // Аккордион
 import menu from './modules/menu.js';
-import submenu from './modules/submenu.js';
+// import submenu from './modules/submenu.js';
 import header from './modules/header.js';
 import slider from './modules/slider.js';
 import modal from './modules/modal.js';
 accordion();
 menu();
-submenu();
+// submenu();
 header();
 slider();
 modal();
